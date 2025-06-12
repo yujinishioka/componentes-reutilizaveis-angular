fe0db5a082b624f4ed00435197a683d9a50c0c7f
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2025-06-12 13:47:13 -0300

    feat: componente switch implementado

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CustomSelectComponent } from '../../shared/components/custom-select/custom-select.component';
+import { CustomSwitchComponent } from '../../shared/components/custom-switch/custom-switch';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
-  imports: [FormsModule, CustomSelectComponent],
+  imports: [FormsModule, CustomSelectComponent, CustomSwitchComponent],
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.css'
 })
@@ -17,4 +18,8 @@ export class DemoComponent {
     { value: 'css', label: 'CSS' },
     { value: 'js', label: 'JavaScript' }
   ];
+
+  switchState1 = false;
+  switchState2 = false;
+  switchState3 = true;
 }
--- src/app/shared/components/custom-switch/custom-switch.ts
+++ src/app/shared/components/custom-switch/custom-switch.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-switch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './custom-switch.html',
   styleUrl: './custom-switch.css'
 })
 export class CustomSwitchComponent {
+  @Input() disabled = false;
+  @Input() modelValue = false;
+  @Output() modelValueChange = new EventEmitter<boolean>();
 
+  toggle() {
+    if (!this.disabled) {
+      this.modelValue = !this.modelValue;
+      this.modelValueChange.emit(this.modelValue);
+    }
+  }
 }
